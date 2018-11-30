42f4cdc33983a996c1da4b31b729f9d05867ff65
deannamjacobs <jacobs.deanna.m@gmail.com>
2018-11-30 16:22:37 -0500

    add comment and votes to view. create vote button.
    Co-authored-by Deanna Jacobs <jacobs.deanna.m@gmail.com>
    Co-authored-by Sean Houde <seanhoude@gmail.com>

--- linkinator/views.py
+++ linkinator/views.py
@@ -25,14 +25,21 @@ def create_post(request):
     })
 
 def post_view(request, slug):
-    comments = Comment.objects.all().order_by('created')
     post = Post.objects.get(slug=slug)
 
     return render(request, 'post_detail.html', {
-        'comments': comments,
         'post': post,
         'slug': slug,
     })
+
+def vote(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    if request.user in post.votes.all():
+        post.votes.get(user=request.user).delete()
+    else:
+        post.votes.create(user=request.user)
+    return redirect('post_detail', slug)
 
 class PostDetailView(DetailView):
     model = Post
