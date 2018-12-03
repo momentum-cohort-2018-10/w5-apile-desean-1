5242756052f4544f92d95bb83c1e698ffdba977e
Sean Houde <seanhoude@gmail.com>
2018-12-02 21:47:24 -0500

    tweaks for heroku

--- apile/heroku_settings.py
+++ apile/heroku_settings.py
@@ -2,7 +2,6 @@ from apile.settings import *
 
 DEBUG = False
 
-STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://serene-river-53724.herokuapp.com/', 'https://arcane-mesa-74274.herokuapp.com/']
