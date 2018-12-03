146bbe5bc55f461178356c1253f696f0a21f6e35
Sean Houde <seanhoude@gmail.com>
2018-12-02 21:42:45 -0500

    Add heroku url's to allowed hosts '

--- apile/settings.py
+++ apile/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'm%qc09ky)&fehtg5e5(r8a_2-ab=1ce=%v&*b3np6-t@thf8l!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://serene-river-53724.herokuapp.com/', 'https://arcane-mesa-74274.herokuapp.com/']
 
 
 # Application definition
